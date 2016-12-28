66bc9b0c9e4434ce96bd98748e94f6664d04a7ae
ramirezr69 <ronramirez@patricks-imac.attlocal.net>
2016-12-27 19:49:55 -0600

    Setup UI

---
+++ Rainy Shine/WeatherVC.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Rainy Shine
+//
+//  Created by Ron Ramirez on 12/27/16.
+//  Copyright © 2016 Mochi Apps. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var currentLocationLabel: UILabel!
+    @IBOutlet weak var currentWeatherIconImage: UIImageView!
+    @IBOutlet weak var currentWeatherLabel: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath)
+        return cell
+    }
+
+
+}
+
